481bc0e4f25504616088750ecf215f22ef840d43
Godbell <12191579@inha.edu>
2024-12-26 13:02:47 +0900

    Fix: edit google trend api usage

--- src/GoogleTrends.js
+++ src/GoogleTrends.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import Script from "react-load-script";
 
 export default function GoogleTrends({ type, keyword, url }) {
+  useEffect(() => {
+    setTimeout(() => {
+        window.trends.embed.renderExploreWidgetTo(
+            document.getElementById("widget"),
+            type,
+            {
+              comparisonItem: [{ keyword, geo: "KR", time: "today 12-m" }],
+              category: 0,
+              property: ""
+            },
+            {
+              exploreQuery: `q=${encodeURI(keyword)}&geo=US&date=today 12-m`,
+              guestPath: "https://trends.google.com:443/trends/embed/"
+            }
+          );
+    }, 500)
+  }, []);
   const handleScriptLoad = _ => {
-    window.trends.embed.renderExploreWidgetTo(
-      document.getElementById("widget"),
-      type,
-      {
-        comparisonItem: [{ keyword, geo: "KR", time: "today 12-m" }],
-        category: 0,
-        property: ""
-      },
-      {
-        exploreQuery: `q=${encodeURI(keyword)}&geo=US&date=today 12-m`,
-        guestPath: "https://trends.google.com:443/trends/embed/"
-      }
-    );
+    
   };
 
   const renderGoogleTrend = _ => {
